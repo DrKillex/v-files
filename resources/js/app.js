cce28112f7bab4da9a874682719a70e0aefe28ed
DrKillex <115083058+DrKillex@users.noreply.github.com>
2023-05-15 16:46:20 +0200

    Initial commit

---
+++ resources/js/app.js
@@ -0,0 +1,10 @@
+import './bootstrap';
+import '~resources/scss/app.scss'
+
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
+
+// gestione immagini build
+import.meta.glob([
+    '../img/**'
+])
---
+++ resources/views/home.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Laravel</title>
+         @vite('resources/js/app.js')
+    </head>
+    <body>
+        <div class="container">
+            <h1>Template Classe 89</h1>
+            <img src="{{ Vite::asset('resources/img/pluto.webp') }}" alt="" srcset="">
+        </div>
+
+    </body>
+</html>
