035dbe5cf4699b18cf04037117c5dd7a11508b9c
Herenyon <rikitur17@gmail.com>
2023-05-15 18:04:04 +0200

    modifica nav

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -2,10 +2,10 @@
     
     <div class="container text-center">
         <h1>Header</h1>
-        <div>
-            <a href="#">home</a>
-            <a href="#">create</a>
-        </div>
+        <ul class="d-flex list-unstyled gap-2 justify-content-center">
+            <li><a href="#">home</a></li>  
+            <li><a href="#">create</a></li> 
+        </ul>
         
     </div>
 </header>
