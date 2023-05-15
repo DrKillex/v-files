05fb8fc8f76d93d8f4d2f78ee4293e1175d4aaf4
albertogioia93 <albertogioia@hotmail.it>
2023-05-15 21:12:45 +0200

    feat: delete

--- app/Http/Controllers/Admin/GamesController.php
+++ app/Http/Controllers/Admin/GamesController.php
@@ -80,8 +80,9 @@ class GamesController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function destroy($id)
+    public function destroy(Game $game)
     {
-        //
+        $game->delete();
+        return to_route('games.index');
     }
 }
