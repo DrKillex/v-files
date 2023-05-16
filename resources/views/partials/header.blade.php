dcb73ce43351900e8a605498d12514a1e3117318
DrKillex <lcollaro1994@gmail.com>
2023-05-16 20:09:32 +0200

    aggiunta link header

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -3,8 +3,8 @@
     <div class="container text-center">
         <h1>Header</h1>
         <ul class="d-flex list-unstyled gap-2 justify-content-center">
-            <li><a href="#">home</a></li>  
-            <li><a href="#">create</a></li> 
+            <li><a href="{{ route('games.index') }}">home</a></li>  
+            <li><a href="{{ route('games.create') }}">create</a></li> 
         </ul>
         
     </div>
