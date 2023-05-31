cfb009c29b1c1524fea4ac6bb0c57911cfbdf1bd
DrKillex <lcollaro1994@gmail.com>
2023-05-31 10:21:48 +0200

    small fix

--- database/seeders/DeveloperTableSeeder.php
+++ database/seeders/DeveloperTableSeeder.php
@@ -6,6 +6,7 @@ use App\Models\Developer;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Schema;
 
 class DeveloperTableSeeder extends Seeder
 {
@@ -16,8 +17,10 @@ class DeveloperTableSeeder extends Seeder
      */
     public function run()
     {
-        Developer::truncate();
         $name=['Variant','Moburst','Clay','SmartSites','PurpleFire','Neuron','Clever Code Lab','Konstant Infosolutions'];
+        Schema::disableForeignKeyConstraints();
+        Developer::truncate();
+        Schema::enableForeignKeyConstraints();
         foreach($name as $item){
             $developer=new Developer();
             $developer->name=$item;
