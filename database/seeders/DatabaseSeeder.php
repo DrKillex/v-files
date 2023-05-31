fb2c4184311b34f51d3e5aa7ff67426def5fd068
DrKillex <lcollaro1994@gmail.com>
2023-05-31 10:26:13 +0200

    all seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([           
             GenreSeeder::class,
+            DeveloperTableSeeder::class,
             GamesTableSeeder::class,
         ]);
     }
